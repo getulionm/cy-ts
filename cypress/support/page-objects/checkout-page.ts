dae7596943633d485eddca9a08b4c61826582f47
Getulio Gasparotto Dadald <getulio@busuu.com>
2023-11-16 18:23:33 +0000

    chore(): remove saucedemo cases

--- cypress/support/page-objects/checkout-page.ts
+++
@@ -1,8 +0,0 @@
-class Checkout {
-  title: string = 'Checkout: Your Information';
-  url: string = Cypress.config().baseUrl + '/checkout-step-one.html';
-
-  get checkoutForm(): Cypress.Chainable<JQuery<HTMLElement>> { return cy.get('#checkout_info_container'); }
-}
-
-export default new Checkout();
--- cypress/support/page-objects/header-component.ts
+++
@@ -1,8 +0,0 @@
-class Header {
-  get pageTitle(): Cypress.Chainable<JQuery<HTMLElement>> { return cy.get('#header_container .title'); }
-  get cartIcon(): Cypress.Chainable<JQuery<HTMLElement>> { return cy.get('#shopping_cart_container'); }
-  get cartIconBadge(): Cypress.Chainable<JQuery<HTMLElement>> { return cy.get('.shopping_cart_badge'); }
-  get sortDropdown(): Cypress.Chainable<JQuery<HTMLElement>> { return cy.get('[data-test="product_sort_container"]'); }
-}
-
-export default new Header();
